a5e22991d034cfb9ba3aebf4e61fea5dd39a1009
artemshibaev <inkognida.art@gmail.com>
2023-01-30 03:27:03 +0300

    start

---
+++ Makefile
@@ -0,0 +1,24 @@
+SRCS = ./srcs/main.cpp
+
+CPP = c++
+
+CPPFLAGS = -Wall -Wextra -Werror -std=c++98
+
+HEADERS = ./headers/*.hpp
+
+NAME = ircserv
+
+
+all: $(NAME)
+
+$(NAME): $(SRCS) $(HEADERS)
+			$(CPP) $(CPPFLAGS) -o $(NAME) $(SRCS)
+
+fclean: rm -f $(NAME)
+
+clean: fclean
+
+re: fclean all
+
+
+.PHONY: re clean fclean all
---
+++ srcs/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+
+struct port_password {
+    int         port;
+    std::string password;
+};
+
+port_password parser(int argc, char **argv) {
+    port_password input;
+    int port;
+
+    port = std::atoi(argv[argc - 2]);
+
+    if (port >= 1 && port <= 65535 && argv[argc - 1]) {
+        input.port = port;
+        input.password = argv[argc - 1];
+    } else {
+        std::cout << "Wrong port or empty password" << std::endl;
+        exit(1);
+    }
+
+    return input;
+}
+
+int main(int argc, char **argv) {
+
+    if (argc != 3) {
+        std::cout << "Wrong input" << std::endl;
+        return 1;
+    }
+
+    port_password input = parser(argc, argv);
+
+    return 0;
+}
